33d1891ddba8a558bca3f0231512bc2a878cf2cd
Nursing <namar.nursing.edu@gmail.com>
2026-09-26 14:00:38 +0300

    Add files via upload

--- jci-tracer/service-worker.js
+++ jci-tracer/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='jci-tracer-shell-v0.6.0';
+const CACHE='jci-tracer-shell-v0.6.3';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./JCI_Tracer_Workflow_Pilot_DB_v0_7.json','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
